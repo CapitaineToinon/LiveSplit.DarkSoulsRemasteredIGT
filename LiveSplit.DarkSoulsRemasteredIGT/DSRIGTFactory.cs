35efdd46a9bbe6379840341a08a8cded12336a68
CapitaineToinon <antoinesutter@gmail.com>
2018-05-25 23:10:43 +0200

    Trying again

--- LiveSplit.DarkSoulsRemasteredIGT/DSRIGTFactory.cs
+++ LiveSplit.DarkSoulsRemasteredIGT/DSRIGTFactory.cs
@@ -15,7 +15,7 @@ namespace LiveSplit.DarkSoulsRemasteredIGT
 
         public ComponentCategory Category => ComponentCategory.Timer;
         public string UpdateURL => "https://raw.githubusercontent.com/CapitaineToinon/LiveSplit.DarkSoulsRemasteredIGT/livesplit-update";
-        public string XMLURL => UpdateURL + "LiveSplit.DarkSoulsRemasteredIGT/Components/update.LiveSplit.DarkSoulsRemastered.xml";
+        public string XMLURL => UpdateURL + "/LiveSplit.DarkSoulsRemasteredIGT/Components/update.LiveSplit.DarkSoulsRemastered.xml";
 
         public Version Version => Assembly.GetExecutingAssembly().GetName().Version;
 
